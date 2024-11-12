2ba7e16306a11ad608af594789c2d32827b0f24d
TunMod <tunmodsupp@gmail.com>
2024-11-12 22:49:09 +0330

    Add files via upload

--- TunMod VPN Client/Properties/AssemblyInfo.cs
+++ TunMod VPN Client/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TunMod VPN Client")]
+[assembly: AssemblyDescription("TunMod co.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TunMod co.")]
+[assembly: AssemblyProduct("TunMod VPN Client")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed83f7c5-778a-45c8-9041-27986930e2e5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.6.1")]
+[assembly: AssemblyFileVersion("4.6.1")]
