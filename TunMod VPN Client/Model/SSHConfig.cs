2562cc1826958f6d375210270eb50c67fae188eb
TunMod <tunmodsupp@gmail.com>
2024-11-12 22:44:16 +0330

    Add files via upload

--- TunMod VPN Client/Model/SSHConfig.cs
+++ TunMod VPN Client/Model/SSHConfig.cs
@@ -1,1 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace TunMod_VPN_Client.Model
+{
+    public class SSHConfig
+    {
+        public long id { get; set; }
+
+        public string C_Name { get; set; }
+
+        public string C_Host { get; set; }
+
+        public int C_Port { get; set; }
+
+        public int C_UDPGW { get; set; }
+
+        public string C_User { get; set; }
+
+        public string C_Pass { get; set; }
+
+        public Boolean C_Default { get; set; }
+
+
+    }
+}
